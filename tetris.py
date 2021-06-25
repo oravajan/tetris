046f126e126c9aa2b160c12c398ea42d02c21f8f
oravajan <oravajan@fit.cvut.cz>
2021-06-25 14:58:35 +0200

    Added main menu.

--- tetris.py
+++ tetris.py
@@ -2,7 +2,7 @@
 # By Jan Orava
 
 import pyglet
-
+from pyglet.window import key
 
 # -------------------------------------------------------------------------------------------------------------------- #
 #                                                 CONSTANT DECLARATION                                                 #
@@ -19,12 +19,80 @@ WINDOW_TITLE = "Tetris"
 # -------------------------------------------------------------------------------------------------------------------- #
 #                                                 MENU CLASS                                                           #
 # -------------------------------------------------------------------------------------------------------------------- #
-class MainMenu:
-    def __init__(self):
-        pass
-
+class Overlay:
     def draw(self):
         pass
+
+
+class Menu(Overlay):
+    def __init__(self, title):
+        self.items = []
+        self.title_text = pyglet.text.Label(title,
+                                            font_name='Verdana',
+                                            font_size=36,
+                                            x=WINDOW_WIDTH // 2,
+                                            y=350,
+                                            anchor_x='center',
+                                            anchor_y='center')
+        self.selected_index = 0
+
+    def reset(self):
+        self.selected_index = 0
+
+    def on_key_press(self, symbol, modifiers):
+        if symbol == key.DOWN:
+            self.selected_index += 1
+        elif symbol == key.UP:
+            self.selected_index -= 1
+        elif symbol == key.ENTER:
+            self.items[self.selected_index].on_key_press(symbol, modifiers)
+
+        if self.selected_index > len(self.items) - 1:
+            self.selected_index = 0
+        if self.selected_index < 0:
+            self.selected_index = len(self.items) - 1
+
+    def draw(self):
+        self.title_text.draw()
+        for index, item in enumerate(self.items):
+            if index == self.selected_index:
+                item.draw(True)
+            else:
+                item.draw(False)
+
+
+class MenuItem:
+    def __init__(self, label, y, activate_func):
+        self.text = pyglet.text.Label(label,
+                                      font_name='Verdana',
+                                      font_size=14,
+                                      x=WINDOW_WIDTH // 2,
+                                      y=y,
+                                      anchor_x='center',
+                                      anchor_y='center')
+        self.activate_func = activate_func
+
+    def draw(self, selected):
+        if selected:
+            self.text.color = (255, 0, 0, 255)
+        else:
+            self.text.color = (255, 255, 255, 255)
+        self.text.draw()
+
+    def on_key_press(self, symbol, modifiers):
+        if self.activate_func:
+            self.activate_func()
+
+
+class MainMenu(Menu):
+    def __init__(self):
+        super(MainMenu, self).__init__('Tetris')
+
+        self.items.append(MenuItem('New Game', 240, None))
+        self.items.append(MenuItem('Load Game', 200, None))
+        self.items.append(MenuItem('Leaderboard', 160, None))
+        self.items.append(MenuItem('Quit Game', 120, pyglet.app.exit))
+        self.reset()
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -48,6 +116,7 @@ def main():
 #                                                 GLOBAL VARIABLES                                                     #
 # -------------------------------------------------------------------------------------------------------------------- #
 game_window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+overlay = MainMenu()
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
@@ -56,11 +125,12 @@ game_window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 @game_window.event
 def on_draw():
     game_window.clear()
+    overlay.draw()
 
 
 @game_window.event
 def on_key_press(symbol, modifiers):
-    pass
+    overlay.on_key_press(symbol, modifiers)
 
 
 # -------------------------------------------------------------------------------------------------------------------- #
