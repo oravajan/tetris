e448e76efa287100fde88dbeaec4082cd6f7dedb
oravajan <oravajan@fit.cvut.cz>
2021-07-01 15:02:18 +0200

    Edited drawing of next block.

--- tetris.py
+++ tetris.py
@@ -5,7 +5,6 @@ import random
 import pyglet
 
 from pyglet.window import key
-from pyglet.gl import *
 
 # -------------------------------------------------------------------------------------------------------------------- #
 #                                                 CONSTANT DECLARATION                                                 #
@@ -198,10 +197,6 @@ class Block:
                           [1, 1]]
             self.img = pyglet.sprite.Sprite(tetris_img_grid[6], 0, 0)
 
-        self.x = PLAY_GRID_WIDTH + 4 - len(self.shape) // 2
-        self.y = PLAY_GRID_HEIGHT * 3 // 4 - 3
-
-    def set_spawn_pos(self):
         self.x = PLAY_GRID_WIDTH // 2 - len(self.shape) // 2
         self.y = PLAY_GRID_HEIGHT - 2
 
@@ -210,9 +205,9 @@ class Block:
         y = self.y
         for row, e in reversed(list(enumerate(self.shape))):
             for col in range(len(self.shape[row])):
-                self.img.x = x * (TILE_SIZE + 1) + self.grid_start_x
-                self.img.y = y * (TILE_SIZE + 1) + self.grid_start_y
                 if self.shape[row][col] == 1:
+                    self.img.x = x * (TILE_SIZE + 1) + self.grid_start_x
+                    self.img.y = y * (TILE_SIZE + 1) + self.grid_start_y
                     self.img.draw()
                 # else:
                 #     pyglet.shapes.Rectangle(x * (TILE_SIZE + 1) + self.grid_start_x,
@@ -221,6 +216,14 @@ class Block:
                 x += 1
             y += 1
             x = self.x
+
+    def draw_as_next(self):
+        for row, e in reversed(list(enumerate(self.shape))):
+            for col in range(len(self.shape[row])):
+                if self.shape[row][col] == 1:
+                    self.img.x = col * (TILE_SIZE + 1) + next_block_label.x - len(self.shape[0])/2 * (TILE_SIZE + 1)
+                    self.img.y = (len(self.shape) - row - 4) * (TILE_SIZE + 1) + next_block_label.y
+                    self.img.draw()
 
     def move_left(self, grid):
         x = self.x
@@ -297,12 +300,12 @@ class Grid:
     def draw(self):
         x = self.start_x
         for cols in range(self.width + 1):
-            pyglet.shapes.Line(x, self.start_y, x, self.end_y).draw()
+            pyglet.shapes.Line(x, self.start_y, x, self.end_y-1).draw()
             x += self.tile_size + 1
 
         y = self.start_y
         for rows in range(self.height + 1):
-            pyglet.shapes.Line(self.start_x, y, self.end_x, y).draw()
+            pyglet.shapes.Line(self.start_x - 1, y, self.end_x, y).draw()
             y += self.tile_size + 1
 
         for row in range(self.height):
@@ -365,19 +368,12 @@ class Game:
                          WINDOW_WIDTH // 2 - (PLAY_GRID_WIDTH * (TILE_SIZE+1) + 1) // 2,
                          WINDOW_OFFSET)
         self.block = Block(self.grid.start_x, self.grid.start_y)
-        self.block.set_spawn_pos()
         self.next_block = Block(self.grid.start_x, self.grid.start_y)
         self.speed = GAME_SPEED
 
     def update(self, dt):
         if not self.block.move_down(self.grid):
-            self.grid.add_block(self.block)
-            self.grid.check_rows()
-
-            self.block = self.next_block
-            self.block.set_spawn_pos()
-
-            self.next_block = Block(self.grid.start_x, self.grid.start_y)
+            self.block_fell()
 
             if not self.grid.is_free(self.block.shape, self.block.x, self.block.y):
                 # losing game, new block can not be spawned
@@ -387,7 +383,7 @@ class Game:
     def draw(self):
         self.grid.draw()
         self.block.draw()
-        self.next_block.draw()
+        self.next_block.draw_as_next()
 
     def pause(self):
         self.running = False
@@ -415,9 +411,10 @@ class Game:
         global score
         self.grid.reset()
         self.speed = GAME_SPEED
+
         self.block = Block(self.grid.start_x, self.grid.start_y)
-        self.block.set_spawn_pos()
         self.next_block = Block(self.grid.start_x, self.grid.start_y)
+
         score = 0
         self.unpause()
 
@@ -427,20 +424,24 @@ class Game:
             pyglet.clock.unschedule(self.update)
             pyglet.clock.schedule_interval(self.update, 1 / self.speed)
 
+    def block_fell(self):
+        self.grid.add_block(self.block)
+        self.grid.check_rows()
+
+        self.block = self.next_block
+        self.next_block = Block(self.grid.start_x, self.grid.start_y)
+
 
 # -------------------------------------------------------------------------------------------------------------------- #
 #                                                 MAIN FUNCTION                                                        #
 # -------------------------------------------------------------------------------------------------------------------- #
 def main():
-    global overlay
+    set_overlay(main_menu)
 
-    glEnable(GL_BLEND)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-
-    overlay = main_menu
     window_x = (pyglet.canvas.Display().get_screens()[0].width - WINDOW_WIDTH) // 2
     window_y = (pyglet.canvas.Display().get_screens()[0].height - WINDOW_HEIGHT) // 2
     window.set_location(window_x, window_y)
+
     pyglet.app.run()
 
 
@@ -481,15 +482,23 @@ def set_clear_overlay():
 #                                                 GLOBAL VARIABLES                                                     #
 # -------------------------------------------------------------------------------------------------------------------- #
 window = pyglet.window.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
+
 try:
-    bg = pyglet.image.load("resources/background.png")
-    main_menu = MainMenu(bg)
-    pause_menu = PauseMenu(bg)
+    main_menu = MainMenu(pyglet.image.load("resources/background.png"))
+    pause_menu = PauseMenu(pyglet.image.load("resources/background.png"))
     window.set_icon(pyglet.image.load("resources/tetris_icon.ico"))
     tetris_img_grid = pyglet.image.ImageGrid(pyglet.image.load("resources/blocks.png"), 1, 7)
 except pyglet.resource.ResourceNotFoundException as error:
     print(error)
     exit(-1)
+
+score_label = pyglet.text.Label("Score: ", FONT_NAME, FONT_SIZE_SCORE,
+                                x=SCORE_X, y=SCORE_Y,
+                                anchor_x='center')
+next_block_label = pyglet.text.Label("Next block:", FONT_NAME, FONT_SIZE_SCORE,
+                                     x=WINDOW_WIDTH - (WINDOW_WIDTH - PLAY_GRID_WIDTH * (TILE_SIZE + 1) - 1) // 4,
+                                     y=SCORE_Y,
+                                     anchor_x='center')
 overlay = Overlay()
 score = 0
 game = Game()
@@ -501,13 +510,6 @@ game = Game()
 @window.event
 def on_draw():
     window.clear()
-    score_label = pyglet.text.Label("Score: ", FONT_NAME, FONT_SIZE_SCORE,
-                                    x=SCORE_X, y=SCORE_Y,
-                                    anchor_x='center')
-    next_block_label = pyglet.text.Label("Next block:", FONT_NAME, FONT_SIZE_SCORE,
-                                         x=WINDOW_WIDTH - (WINDOW_WIDTH - PLAY_GRID_WIDTH * (TILE_SIZE + 1) - 1) // 4,
-                                         y=SCORE_Y,
-                                         anchor_x='center')
 
     if game.running:
         game.draw()
